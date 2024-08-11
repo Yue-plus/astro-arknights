eff82122fecce86688879d40373c3cbe9e84c5f7
Yue-plus <Yue_plus@foxmail.com>
2024-08-11 23:09:27 +0800

    fix(NavItem): liClassName

--- src/components/header/NavItem.tsx
+++ src/components/header/NavItem.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from "react";
 import {useStore} from "@nanostores/react";
 import {viewIndex} from "../store/rootLayoutStore.ts";
 
@@ -6,9 +7,12 @@ export type NavItemInfo = { title: string, subtitle: string, href: string }
 export default function NavItem({index, info}: {index: number, info: NavItemInfo}) {
     const $viewIndex = useStore(viewIndex)
 
-    return <li className={"inline-block text-center mx-10 duration-300 hover:text-ark-blue"
-                   + ($viewIndex !== index || " text-ark-blue")}
-               onClick={() => viewIndex.set(index)}>
+    const liClassName = useMemo(() => {
+        return "inline-block text-center mx-10 duration-300 hover:text-ark-blue"
+            + ($viewIndex !== index ? "" : " text-ark-blue")
+    }, [$viewIndex])
+
+    return <li className={liClassName} onClick={() => viewIndex.set(index)}>
         <a href={info.href} className="text-left">
             <div className="font-oswaldMedium text-[1.375rem]">{info.title}</div>
             <div className="text-[0.875rem]">{info.subtitle}</div>
