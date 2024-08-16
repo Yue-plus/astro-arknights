3d4c64b221e226c0ea0e3e0cd6b9289c73db5e9c
Yue-plus <Yue_plus@foxmail.com>
2024-08-16 09:08:55 +0800

    perf(InfoLayout.astro): 基础正文样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+    }
+  },
+
   integrations: [react(), tailwind({applyBaseStyles: false})]
 });
