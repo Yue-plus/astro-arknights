d91eaecb0ef9bd99cc97c29857b6811d1775434e
Yue-plus <Yue_plus@foxmail.com>
2024-08-26 17:24:41 +0800

    perf(DocsToolPanel): ToTop

--- src/components/DocsToolPanel.tsx
+++ src/components/DocsToolPanel.tsx
@@ -1,9 +1,10 @@
 import {IconArrow} from "./SvgIcons.tsx";
 
 function ToTop() {
-    return <div className="w-[3.75rem] h-full hover:text-black bg-[#3f3f3f] hover:bg-white hover:pb-4 flex items-center justify-center transition-[color,background-color,padding] duration-300 ease-linear"
-                title="回到顶部" aria-label="回到顶部"
-                onClick={() => console.log("TODO: 回到顶部")}>
+    return <div
+        className="w-[3.75rem] h-full hover:text-black bg-[#3f3f3f] hover:bg-white hover:pb-4 flex items-center justify-center transition-[color,background-color,padding] duration-300 ease-linear"
+        title="回到顶部" aria-label="回到顶部"
+        onClick={() => document.getElementById("docs-content")?.scroll(0, 0)}>
         <IconArrow className="w-3 h-auto -rotate-90 select-none"/>
     </div>
 }
