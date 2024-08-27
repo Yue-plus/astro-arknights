db4ec8e53bd872aa7990d97d6d863a33ac2c64b1
Yue-plus <Yue_plus@foxmail.com>
2024-08-27 22:35:11 +0800

    docs(docs/): Getting Started

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: "one-dark-pro",
-    }
-  },
+    markdown: {
+        shikiConfig: {
+            theme: "one-dark-pro",
+        },
+    },
 
-  integrations: [react(), tailwind({applyBaseStyles: false})]
+    integrations: [react(), tailwind({applyBaseStyles: false})],
 });
