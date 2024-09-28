00ed5d63d54953e1834d89ccf410e124e678a9cb
ChisatoNishikigi73 <lxr2061895177@163.com>
2024-09-29 03:33:13 +0800

    feat: add left line

--- src/components/LineDecorator.tsx
+++ src/components/LineDecorator.tsx
@@ -11,11 +11,13 @@ export default function LineDecorator() {
         base + (right ? "right-[14.75rem] portrait:right-[5.75rem] " : "-right-px ") + "w-px h-full top-0", [right])
     const bottomClassName = useMemo(() =>
         base + (bottom ? "bottom-[11.25rem] portrait:bottom-[12rem] " : "-bottom-px ") + "w-full h-px", [bottom])
-
+    const leftClassName = useMemo(() =>
+        base + (left ? "left-[14.75rem] portrait:left-[5.75rem] " : "-left-px ") + "w-px h-full top-0", [left])
     return <div id="line-decorator"
                 className="w-full h-full absolute top-0 left-0 z-[3] pointer-events-none overflow-hidden">
         <div className={topClassName}/>
         <div className={rightClassName}/>
         <div className={bottomClassName}/>
+        <div className={leftClassName}/>
     </div>
 }
